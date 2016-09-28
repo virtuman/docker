85a1ea458367ecc3f36330862d36ae5ccfc53ab0
Ray Walker <hello@raywalker.it>
2016-09-28 21:08:56 +1000

    change NEWRELIC_APP_NAME to NEWRELIC_APPNAME

--- php-newrelic/bin/init_newrelic.sh
+++ php-newrelic/bin/init_newrelic.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-echo " * newrelic:    app_name$: ${NEWRELIC_APP_NAME}"
-echo " * newrelic:    license:   ${NEWRELIC_LICENSE}"
+echo " * newrelic: app_name:  ${NEWRELIC_APPNAME}"
+echo " * newrelic: license:   ${NEWRELIC_LICENSE}"
 
-sed -i -r "s/^newrelic\.license.+$/newrelic.license=\"${NEWRELIC_LICENSE}\"/g" /etc/php/5.6/fpm/conf.d/newrelic.ini
-sed -i -r "s/^newrelic\.appname.+$/newrelic.license=\"${NEWRELIC_APP_NAME}\"/g" /etc/php/5.6/fpm/conf.d/newrelic.ini
+sed -i -r "s/^newrelic\.license=.+$/newrelic.license=\"${NEWRELIC_LICENSE}\"/g" /etc/php/5.6/fpm/conf.d/newrelic.ini
+sed -i -r "s/^newrelic\.appname=.+$/newrelic.license=\"${NEWRELIC_APPNAME}\"/g" /etc/php/5.6/fpm/conf.d/newrelic.ini
